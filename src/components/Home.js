85f65ae47d2bd5b00e26183e6f87b5a03532f494
snehalghule <seeghule@gmail.com>
2020-03-09 21:13:37 +0100

    Home Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './Home.css'
+import Slider from "react-slick";
 
-export default function Home() {
+const photos = [
+  {
+    name:'photo 1',
+    url:"/img/t.jpg"
+  },
+  {
+    name:'photo 2',
+    url:"/img/o.jpg"
+  },
+  {
+    name:'photo 3',
+    url:"/img/th.jpg"
+  },
+  {
+    name:'photo 4',
+    url:"/img/fr.jpg"
+  },
+  {
+    name:'photo 5',
+    url:"/img/fv.jpg"
+  }
+]
+    class SimpleSlider extends Component {
+    render(){
+    const settings ={
+            infinite: true,
+            slidesToShow: 1, 
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 2000,
+            autoplaySpeed: 5000,
+            fade: true,
+            pauseOnHover: true,
+            cssEase: "linear",
+            className: "slides"
+            }
     return (
-        <div>
-            <h1>Hello from Home</h1>
-        </div>
-    )
-}
+           <div className="SimpleSlider">
+            <Slider {...settings}>
+            {photos.map((photo) => {
+            return(
+              <div>
+               <img src={photo.url} className="clsImage"/>
+              </div>
+            )
+            })}
+            </Slider>
+            <h1 className="text">Welcome To Indian Spices</h1>
+            </div>
+            );
+      }
+    } 
+export default SimpleSlider;
+
+
+
+
+
