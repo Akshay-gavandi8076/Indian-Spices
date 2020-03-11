fe2dd2c12274f01e5ff55b17f21d4cbcb6759e61
snehal9131 <58152105+snehal9131@users.noreply.github.com>
2020-03-12 00:04:53 +0100

    added login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm';
+import './Login.css';
 
-export default function Login() {
+
+
+class Login extends Component {
+    render(){
     return (
-        <div>
-            <h1>Hello from login</h1>
+    <Router>
+        <div className="Login">
+        <div className="Login__Form">
+
+        <div className="PageSwitcher">
+            <NavLink exact to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+            <NavLink  to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
         </div>
-    )
+
+        <div className="FormTitle">
+            <NavLink exact to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink to="/sign-up" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+        </div>
+        <Route path="/sign-in" component={SignInForm} >
+          </Route>
+        
+        <Route path="/sign-up" component={SignUpForm}>
+        </Route>
+        </div>
+      </div>
+    </Router>
+    );
+ }
 }
+export default Login;
