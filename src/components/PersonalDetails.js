2f2069efb4507c3acc4e96c4368fd05501f6885b
snehal9131 <58152105+snehal9131@users.noreply.github.com>
2020-03-12 01:33:50 +0100

    created Customer Details Page

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './PersonalDetails.css';
 
-export default function PersonalDetails() {
-    return (
-        <div className="col-10 mx-auto col-lg-6 text-center text-capitalize p-5">
-            
-            <h5 className="text-title">enter details</h5>
-            
-        </div>
-    )
+
+class PersonalDetails extends Component{
+    render(){
+        return(
+            <div className="FormCenter">
+            <form className="FormFields" onSubmit={this.handelSubmit}>
+
+            <h2 className="head">Customer Details</h2>
+
+            <div className="FormField">
+                <label className="FormField__Label" htmlFor="name">Full Name</label>
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your Full name" name="name"/>
+            </div>
+            <div className="FormField">
+                <label className="FormField__Label" htmlFor="address">Address</label>
+                <input type="text" id="address" className="FormField__Input" placeholder="Enter your Address" name="address"/>
+            </div>
+
+            <div className="FormField">
+                <label className="FormField__Label" htmlFor="number">Mobile No</label>
+                <input type="number" id="number" className="FormField__Input" placeholder="Enter your Mobile No" name="number"/>
+            </div>
+
+            <div className="FormField">
+            <label className="FormField__CheckboxLabel">
+                <input className="FormField__CheckboxLabel" type="checkbox" name="hasAgreed"/> Cash On Delievery </label>
+            </div>
+
+            <div className="FormField">
+                <button className="FormField__Button" >Submit</button> 
+            </div>
+
+            </form> 
+          </div>
+        );
+    }
+
 }
+export default PersonalDetails;
